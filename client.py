f5648dd9b76ea4fc176e206792c33dae6fab5e5b
zed-chi <zed-chi@mail.ru>
2021-02-13 23:44:57 +0300

    added ping to message screen

--- client.py
+++ client.py
@@ -1,11 +1,28 @@
 import asyncio
 import gui
+import time
 
 
-loop = asyncio.get_event_loop()
+async def main():
+    loop = asyncio.get_event_loop()
 
-messages_queue = asyncio.Queue()
-sending_queue = asyncio.Queue()
-status_updates_queue = asyncio.Queue()
+    messages_queue = asyncio.Queue()
+    sending_queue = asyncio.Queue()
+    status_updates_queue = asyncio.Queue()
+    
+    await asyncio.gather(
+        generate_msgs(messages_queue),
+        gui.draw(messages_queue, sending_queue, status_updates_queue),
+        loop=loop
+    )
+    
 
-loop.run_until_complete(gui.draw(messages_queue, sending_queue, status_updates_queue))
+async def generate_msgs(q):
+    while True:
+        msg = round(time.time())
+        q.put_nowait(msg)        
+        await asyncio.sleep(1)
+        
+
+if __name__ == "__main__":
+    asyncio.run(main())
