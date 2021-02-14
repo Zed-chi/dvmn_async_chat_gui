ddda5d9d869684dc8100d06323e087459245b81b
zed-chi <zed-chi@mail.ru>
2021-02-14 14:42:20 +0300

    linter fix

--- gui.py
+++ gui.py
@@ -9,18 +9,18 @@ class TkAppClosed(Exception):
 
 
 class ReadConnectionStateChanged(Enum):
-    INITIATED = 'устанавливаем соединение'
-    ESTABLISHED = 'соединение установлено'
-    CLOSED = 'соединение закрыто'
+    INITIATED = "устанавливаем соединение"
+    ESTABLISHED = "соединение установлено"
+    CLOSED = "соединение закрыто"
 
     def __str__(self):
         return str(self.value)
 
 
 class SendingConnectionStateChanged(Enum):
-    INITIATED = 'устанавливаем соединение'
-    ESTABLISHED = 'соединение установлено'
-    CLOSED = 'соединение закрыто'
+    INITIATED = "устанавливаем соединение"
+    ESTABLISHED = "соединение установлено"
+    CLOSED = "соединение закрыто"
 
     def __str__(self):
         return str(self.value)
@@ -51,34 +51,34 @@ async def update_conversation_history(panel, messages_queue):
     while True:
         msg = await messages_queue.get()
 
-        panel['state'] = 'normal'
-        if panel.index('end-1c') != '1.0':
-            panel.insert('end', '\n')
-        panel.insert('end', msg)        
+        panel["state"] = "normal"
+        if panel.index("end-1c") != "1.0":
+            panel.insert("end", "\n")
+        panel.insert("end", msg)
         # TODO сделать промотку умной, чтобы не мешала просматривать историю сообщений
         # ScrolledText.frame
         # ScrolledText.vbar
         panel.yview(tk.END)
-        panel['state'] = 'disabled'
+        panel["state"] = "disabled"
 
 
 async def update_status_panel(status_labels, status_updates_queue):
     nickname_label, read_label, write_label = status_labels
 
-    read_label['text'] = f'Чтение: нет соединения'
-    write_label['text'] = f'Отправка: нет соединения'
-    nickname_label['text'] = f'Имя пользователя: неизвестно'
+    read_label["text"] = "Чтение: нет соединения"
+    write_label["text"] = "Отправка: нет соединения"
+    nickname_label["text"] = "Имя пользователя: неизвестно"
 
     while True:
         msg = await status_updates_queue.get()
         if isinstance(msg, ReadConnectionStateChanged):
-            read_label['text'] = f'Чтение: {msg}'
+            read_label["text"] = f"Чтение: {msg}"
 
         if isinstance(msg, SendingConnectionStateChanged):
-            write_label['text'] = f'Отправка: {msg}'
+            write_label["text"] = f"Отправка: {msg}"
 
         if isinstance(msg, NicknameReceived):
-            nickname_label['text'] = f'Имя пользователя: {msg.nickname}'
+            nickname_label["text"] = f"Имя пользователя: {msg.nickname}"
 
 
 def create_status_panel(root_frame):
@@ -88,13 +88,19 @@ def create_status_panel(root_frame):
     connections_frame = tk.Frame(status_frame)
     connections_frame.pack(side="left")
 
-    nickname_label = tk.Label(connections_frame, height=1, fg='grey', font='arial 10', anchor='w')
+    nickname_label = tk.Label(
+        connections_frame, height=1, fg="grey", font="arial 10", anchor="w",
+    )
     nickname_label.pack(side="top", fill=tk.X)
 
-    status_read_label = tk.Label(connections_frame, height=1, fg='grey', font='arial 10', anchor='w')
+    status_read_label = tk.Label(
+        connections_frame, height=1, fg="grey", font="arial 10", anchor="w",
+    )
     status_read_label.pack(side="top", fill=tk.X)
 
-    status_write_label = tk.Label(connections_frame, height=1, fg='grey', font='arial 10', anchor='w')
+    status_write_label = tk.Label(
+        connections_frame, height=1, fg="grey", font="arial 10", anchor="w",
+    )
     status_write_label.pack(side="top", fill=tk.X)
 
     return (nickname_label, status_read_label, status_write_label)
@@ -103,7 +109,7 @@ def create_status_panel(root_frame):
 async def draw(messages_queue, sending_queue, status_updates_queue):
     root = tk.Tk()
 
-    root.title('Чат Майнкрафтера')
+    root.title("Чат Майнкрафтера")
 
     root_frame = tk.Frame()
     root_frame.pack(fill="both", expand=True)
@@ -116,18 +122,23 @@ async def draw(messages_queue, sending_queue, status_updates_queue):
     input_field = tk.Entry(input_frame)
     input_field.pack(side="left", fill=tk.X, expand=True)
 
-    input_field.bind("<Return>", lambda event: process_new_message(input_field, sending_queue))
+    input_field.bind(
+        "<Return>",
+        lambda event: process_new_message(input_field, sending_queue),
+    )
 
     send_button = tk.Button(input_frame)
     send_button["text"] = "Отправить"
-    send_button["command"] = lambda: process_new_message(input_field, sending_queue)
+    send_button["command"] = lambda: process_new_message(
+        input_field, sending_queue,
+    )
     send_button.pack(side="left")
 
-    conversation_panel = ScrolledText(root_frame, wrap='none')
+    conversation_panel = ScrolledText(root_frame, wrap="none")
     conversation_panel.pack(side="top", fill="both", expand=True)
 
     await asyncio.gather(
         update_tk(root_frame),
         update_conversation_history(conversation_panel, messages_queue),
-        update_status_panel(status_labels, status_updates_queue),       
+        update_status_panel(status_labels, status_updates_queue),
     )
