5c3848d334ab4b7c7a06779fb9d9bd29c359b6f5
zed-chi <zed-chi@mail.ru>
2021-02-14 11:20:50 +0300

    added read_msgs func

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import asyncio
 import gui
 import time
 from listen_minechat import get_args as view_args
+from listen_minechat import read_msgs
 
 
 async def main():
@@ -13,7 +14,8 @@ async def main():
     status_updates_queue = asyncio.Queue()
     
     await asyncio.gather(
-        generate_msgs(messages_queue),
+        #generate_msgs(messages_queue),
+        read_msgs(args.host, args.port, messages_queue),
         gui.draw(messages_queue, sending_queue, status_updates_queue),
         loop=loop
     )
@@ -26,5 +28,5 @@ async def generate_msgs(q):
         await asyncio.sleep(1)
         
 
-if __name__ == "__main__":
+if __name__ == "__main__":        
     asyncio.run(main())
