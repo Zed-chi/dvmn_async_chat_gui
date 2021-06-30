7efed828ca2a6bdef9ec08bbcf2d1c3a83c47064
zed-chi <zed-chi@mail.ru>
2021-06-30 18:57:41 +0300

    naming fixes, added alert messages

--- registration.py
+++ registration.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import tkinter as tk
+from tkinter import Tk, messagebox
 from tkinter.constants import DISABLED, NORMAL
 
 from sender import sanitize
@@ -42,19 +43,22 @@ class RegistrationWindow:
 
     async def register_user(self):
         name = self.name_input.get()
-        r, w = await asyncio.open_connection(self.host, self.port)
+        reader, writer = await asyncio.open_connection(self.host, self.port)
         try:
-            print(await r.readline())
-            w.write("\n".encode())
-            print(await r.readline())
-            w.write(f"{name}\n".encode())
-            response_data = (await r.readline()).decode()
+            await reader.readline()
+            writer.write("\n".encode())
+            await reader.readline()
+            writer.write(f"{name}\n".encode())
+            response_data = (await reader.readline()).decode()
             json_text = response_data.split("\n")[0]
             user_dict = json.loads(json_text)
             self.save_token(user_dict["account_hash"])
+            messagebox.showinfo("Success", "Token created and saved")
+        except Exception as e:
+            messagebox.showinfo("Error", e)
         finally:
-            w.close()
-            await w.wait_closed()
+            writer.close()
+            await writer.wait_closed()
             self.root.destroy()
 
     def save_token(self, account_hash):
