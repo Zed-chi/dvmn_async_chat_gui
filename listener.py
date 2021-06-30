d44a8e27c5d622f0d4b55b69e90b65771172aec1
zed-chi <zed-chi@mail.ru>
2021-06-30 18:57:12 +0300

    removed unnecessery code

--- listener.py
+++ listener.py
@@ -3,66 +3,29 @@ from datetime import datetime
 
 import aiofiles
 
-import configargparse
 
-from gui import ReadConnectionStateChanged
-
-
-CONFIG_FILEPATH = "./listener_config.cfg"
-
-
-def get_args():
-    parser = configargparse.ArgParser(
-        default_config_files=[
-            CONFIG_FILEPATH,
-        ],
-    )
-    parser.add(
-        "--host",
-        required=False,
-        help="host address",
-        default="minechat.dvmn.org",
-    )
-    parser.add(
-        "--port",
-        required=False,
-        help="port of sender client",
-        default=5000,
-    )
-
-    return parser.parse_args()
+async def save_messages(log_path, logging_queue: asyncio.Queue):
+    while True:
+        async with aiofiles.open(
+            log_path,
+            mode="a",
+            encoding="utf-8",
+        ) as f:
+            msg = await logging_queue.get()
+            await f.write(msg + "\n")
 
 
 async def read_msgs(
-    host,
-    port,
-    queue,
-    status_updates_queue,
-    connection_queue,
-    history_path=None,
+    connection,
+    message_queue: asyncio.Queue,
+    logging_queue: asyncio.Queue,
+    watchdog_queue: asyncio.Queue,
 ):
-    status_updates_queue.put_nowait(
-        ReadConnectionStateChanged.INITIATED,
-    )
-    reader, writer = await asyncio.open_connection(host, port)
-    status_updates_queue.put_nowait(
-        ReadConnectionStateChanged.ESTABLISHED,
-    )
-    try:
-        while True:
-            data = await reader.readline()
-            connection_queue.put_nowait("New message in chat")
-
-            message = data.decode("utf-8")
-            now = datetime.now().strftime("[%d.%m.%y %H:%M]")
-            if history_path:
-                async with aiofiles.open(
-                    history_path,
-                    mode="a",
-                    encoding="utf-8",
-                ) as f:
-                    await f.write(f"{now} {message}")
-            queue.put_nowait(message.strip())
-    finally:
-        writer.close()
-        await writer.wait_closed()
+    reader = connection["reader"]
+    while True:
+        data = await asyncio.wait_for(reader.readline(), timeout=5.0)
+        message = data.decode("utf-8")
+        now = datetime.now().strftime("[%d.%m.%y %H:%M]")
+        watchdog_queue.put_nowait("get")
+        message_queue.put_nowait(f"{now} {message.strip()}")
+        logging_queue.put_nowait(f"{now} {message.strip()}")
