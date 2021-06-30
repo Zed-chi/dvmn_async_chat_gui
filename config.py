4d56a39480a0c5f435a87a3df855c9edfd4569fc
zed-chi <zed-chi@mail.ru>
2021-06-30 18:53:26 +0300

    moved some logic to file

---
+++ config.py
@@ -0,0 +1,50 @@
+import configargparse
+
+CONFIG_FILEPATH = "./config.cfg"
+TIMEOUT = 40
+PING_TIMEOUT = 20
+PING_SLEEPTIME = 20
+
+
+
+def get_args():
+    parser = configargparse.ArgParser(
+        default_config_files=[
+            CONFIG_FILEPATH,
+        ],
+    )
+    parser.add(
+        "--host",
+        required=False,
+        help="host address",
+        default="minechat.dvmn.org",
+    )
+    parser.add(
+        "--listener_port",
+        required=False,
+        help="port of sender client",
+        default=5000,
+    )
+    parser.add(
+        "--sender_port",
+        required=False,
+        help="port of sender client",
+        default=5050,
+    )
+    parser.add("--token", help="token", required=False)
+    parser.add(
+        "--history_path",
+        required=False,
+        help="listener log path",
+        default="./history.log",
+    )
+    parser.add(
+        "--sender_log_path",
+        required=False,
+        help="sender log path",
+        default="./sender.log",
+    )
+    return parser.parse_args()
+
+
+args = get_args()
